7e0e5ccd805b16e130a8e074048e983f7c778a60
Armands1214 <kindzulis19@gmail.com>
2025-08-31 21:49:26 +0300

    fix: allow id on <Container>

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,4 +1,14 @@
 import clsx from "clsx";
-export default function Container({ children, className }:{ children: React.ReactNode, className?: string }){
-  return <div className={clsx("mx-auto w-full max-w-6xl px-6", className)}>{children}</div>;
+
+type Props = React.HTMLAttributes<HTMLDivElement> & {
+  className?: string;
+  children: React.ReactNode;
+};
+
+export default function Container({ children, className, ...props }: Props) {
+  return (
+    <div {...props} className={clsx("mx-auto w-full max-w-6xl px-6", className)}>
+      {children}
+    </div>
+  );
 }
